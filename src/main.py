a6ab08cf653cd095fa676d9978b9a75cf7c87c99
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-01-30 21:58:39 -0500

    first commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from mangum import Mangum
+from redis import Redis
+
 from api_v1.api import router as api_router
 
 app = FastAPI(
@@ -7,6 +9,21 @@ app = FastAPI(
     description="This is super fancy, with auto docs and everything!",
     version="0.1.0",
 )
+
+redis = Redis(
+    host="test-redis.kthrlr.ng.0001.use1.cache.amazonaws.com",
+    port=6379,
+    decode_responses=True,
+    ssl=True,
+)
+
+
+@app.get("/redis", name="Redis", tags=["Redis"])
+async def redis_check():
+    if redis.ping():
+        return {"Message": "Connected to Redis"}
+    else:
+        return {"Message": "Could not connect to Redis"}
 
 
 @app.get("/ping", name="Healthcheck", tags=["Healthcheck"])
